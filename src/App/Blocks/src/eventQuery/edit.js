b3e2fc9747a55bc2d91d132bb7e155453742839e
Pea <pea@misfist.com>
2023-04-26 19:37:59 -0400

    Update meta fields

--- src/App/Blocks/src/eventQuery/edit.js
+++ src/App/Blocks/src/eventQuery/edit.js
@@ -515,22 +515,22 @@ const Edit = ( props ) => {
 				) }
 				{ showDate && (
 					<div className="event__date">
-						<time dateTime={ post.meta?.["start_date"] }>{ dateI18n( dateFormat, post.meta?.["start_date"] ) }</time>
+						<time dateTime={ post.meta?.["_event_start"] }>{ dateI18n( dateFormat, post.meta?.["_event_start"] ) }</time>
 					</div>
 				) }
 				{ showTime && (
 					<div className="event__time">
-						<time dateTime={ post.meta?.["start_date"] }>{ dateI18n( timeFormat, post.meta?.["start_date"] ) }</time>
-						{ post.meta?.["end_date"] && showEndTime && (
+						<time dateTime={ post.meta?.["_event_start"] }>{ dateI18n( timeFormat, post.meta?.["_event_start"] ) }</time>
+						{ post.meta?.["_event_end"] && showEndTime && (
 							<>
 								<span className="separator"> - </span>
-								<time dateTime={ post.meta?.["end_date"] }>{ dateI18n( timeFormat, post.meta?.["end_date"] ) }</time>
+								<time dateTime={ post.meta?.["_event_end"] }>{ dateI18n( timeFormat, post.meta?.["_event_end"] ) }</time>
 							</>
 						) }
 					</div>
 					) }
 				{ showLocation && (
-					<div className="event__location" dangerouslySetInnerHTML={{ __html: post.meta?.["location_venue"] }}></div>
+					<div className="event__location" dangerouslySetInnerHTML={{ __html: post.meta?.["_event_location_type"] }}></div>
 				) }
 				</a>
 			</article>
