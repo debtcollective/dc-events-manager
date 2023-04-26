10ce2657569f581aeb78497a900f7d0ec8316cd4
Pea <pea@misfist.com>
2023-04-25 22:23:32 -0400

    Update rest api query vars

--- src/App/Integration/RestFilters.php
+++ src/App/Integration/RestFilters.php
@@ -16,6 +16,8 @@ use DCEventsManager\Common\Abstracts\Base;
  */
 class RestFilters extends Base {
 
+	public const POST_TYPE = 'event';
+
 	/**
 	 * Constructor.
 	 *
@@ -39,9 +41,9 @@ class RestFilters extends Base {
 		 *
 		 * Add plugin code here
 		 */
-		\add_filter( 'rest_query_vars', 										[ $this, 'rest_query_vars' ] );
-		\add_filter( 'rest_' . Event::POST_TYPE['id'] . '_query', 				[ $this, 'rest_query_start_date' ], 10, 2 );
-		\add_filter( 'rest_' . Event::POST_TYPE['id'] . '_collection_params', 	[ $this, 'rest_collection_params' ], 10, 2 );
+		\add_filter( 'rest_query_vars', array( $this, 'rest_query_vars' ) );
+		\add_filter( 'rest_' . self::POST_TYPE . '_query', array( $this, 'rest_query_start_date' ), 10, 2 );
+		\add_filter( 'rest_' . self::POST_TYPE . '_collection_params', array( $this, 'rest_collection_params' ), 10, 2 );
 
 	}
 
@@ -59,31 +61,31 @@ class RestFilters extends Base {
 	/**
 	 * Modify query
 	 * Orderby `meta_value` if `orderby=start` is passed
-	 * 
+	 *
 	 * @see https://developer.wordpress.org/reference/hooks/rest_this-post_type_query/
 	 *
 	 * @param array $params
-	 * @param obj $request
+	 * @param obj   $request
 	 * @return array $params
 	 */
 	function rest_query_start_date( $params, $request ) {
 		if ( isset( $request['orderby'] ) && 'start' === $request['orderby'] ) {
-			$params['orderby'] = 'meta_value';
-			$params['meta_key'] = 'start_date';
+			$params['orderby']  = 'meta_value';
+			$params['meta_key'] = '_event_start';
 		}
 		if ( isset( $request['scope'] ) && 'all' !== $request['scope'] ) {
 			$compare = '>=';
-			if( 'past' === $request['scope'] ) {
+			if ( 'past' === $request['scope'] ) {
 				$compare = '<';
 			}
-			$params['meta_query'] = [
-				[
-					'key' 		=> 'start_date',
-					'value'		=> \date( 'c' ),
-					'compare'	=> $compare,
-					'type'		=> 'DATETIME'
-				]
-			];
+			$params['meta_query'] = array(
+				array(
+					'key'     => '_event_start',
+					'value'   => \date( 'c' ),
+					'compare' => $compare,
+					'type'    => 'DATETIME',
+				),
+			);
 		}
 		return $params;
 	}
@@ -92,26 +94,34 @@ class RestFilters extends Base {
 	 * Register collection parameters
 	 * Add `start` as valid value for `orderby`
 	 * Add `scope` parameter
-	 * 
-	 * 
+	 *
 	 * @see https://developer.wordpress.org/reference/hooks/rest_this-post_type_collection_params/
 	 *
-	 * @param array $params
+	 * @param array  $params
 	 * @param string $post_type
 	 * @return void
 	 */
 	function rest_collection_params( $params, $post_type ) {
-		array_push( $params['orderby']['enum'], 'start' );
-		$params['scope'] = [
+		// array_push( $params['orderby']['enum'], 'start' );
+		$params['scope'] = array(
 			'description' => __( 'Limit scope of events to future or past.', 'dc-events-manager' ),
 			'type'        => 'string',
 			'default'     => 'future',
-			'enum'        => [
+			'enum'        => array(
 				'future',
 				'past',
-				'all'
-			],
-		];
+				'all',
+			),
+		);
+		$params['orderby'] = array(
+			'description' => __( 'Sort by start date or name.', 'dc-events-manager' ),
+			'type'        => 'string',
+			'default'     => 'start',
+			'enum'        => array(
+				'name',
+				'start',
+			),
+		);
 		return $params;
 	}
 
