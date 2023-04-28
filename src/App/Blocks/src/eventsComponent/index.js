a1b7d433c817c68d52e0d85cf85698cdea7d8946
Pea <pea@misfist.com>
2023-04-28 18:13:53 -0400

    Set up events-by-tag as default variation

--- src/App/Blocks/src/eventsComponent/index.js
+++ src/App/Blocks/src/eventsComponent/index.js
@@ -52,12 +52,14 @@
 			 [
 				 'dc-events-manager/event-query',
 				 {
-					 query: {
+					className: 'events-by-tag',
+					query: {
 						 per_page: 3,
 						 order: 'desc',
 						 orderby: 'start'
 					 },
-					 dateFormat: 'l F j, Y',
+					 dateFormat: 'D, M j, Y',
+					 wrapperTagName: 'section',
 					 display: {
 						 showTags: false,
 						 showFeaturedImage: false,
@@ -75,72 +77,13 @@
 			 'inserter',
 			 'transform'
 		 ],
-	 },
-	 {
-		 name: 'events-by-tag-welcome-calls',
-		 title: __( 'Welcome Calls', 'dc-events-manager' ),
-		 description: __( 'Display welcome call events with section header and description..', 'dc-events-manager' ),
-		 icon: 'calendar-alt',
-		 keywords: [
-			 __( 'event', 'dc-events-manager' ),
-			 __( 'grid', 'dc-events-manager' ),
-			 __( 'component', 'dc-events-manager' )
-		 ],
-		 attributes: {
-			 className: 'events-by-tag welcome-calls'
-		 },
-		 innerBlocks: [
-			 [
-				 'core/heading',
-				 {
-					 className: 'taxonomy-label',
-					 level: 2,
-					 placeholder: __( 'Add Title...', 'dc-events-manager' ),
-					 content: __( 'Welcome Calls', 'dc-events-manager' )
-				 },
-			 ],
-			 [
-				 'core/paragraph',
-				 {
-					 className: 'taxonomy-description',
-					 placeholder: __( 'Add Description...', 'dc-events-manager' ),
-					 content: __( 'Are you new to the Debt Collective or the Biden Jubilee 100 campaign and want to learn more about our union and what we\'re fighting for? Join us for our welcome calls!', 'dc-events-manager' )
-				 },
-			 ],
-			 [
-				 'dc-events-manager/event-query',
-				 {
-					 query: {
-						 per_page: 3,
-						 order: 'desc',
-						 orderby: 'start',
-						 'event-tags': [10]
-					 },
-					 eventTags: '10',
-					 dateFormat: 'D, M j',
-					 timeFormat: 'g:ia',
-					 display: {
-						 showTags: false,
-						 showFeaturedImage: false,
-						 showTitle: false,
-						 showDate: true,
-						 showTime: true,
-						 showEndTime: false,
-						 showLocation: true
-					 }
-				 },
-			 ],
-		 ],
-		 scope: [
-			 'transform',
-			 'inserter'
-		 ],
 	 }
  ];
  
  const settings = {
 	 edit: Edit,
 	 save: Save,
+	 icon,
 	 variations
  };
  
