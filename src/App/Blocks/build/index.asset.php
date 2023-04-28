8f9844d80025dd37a7ba9d422721e43b167e86e1
Pea <pea@misfist.com>
2023-04-28 19:31:18 -0400

    Build it

--- src/App/Blocks/build/index.asset.php
+++ src/App/Blocks/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-core-data', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'c8ee6a24a164a3bae5948135d565bb56');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-core-data', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '3222c43b3e1339a475d1ef1bd919f1a7');
