fbf393c124a2f072fe7d07c67d41263e53448f02
Pea <pea@misfist.com>
2023-04-28 19:30:07 -0400

    Add `tagName` selector

--- src/App/Blocks/src/eventsComponent/edit.js
+++ src/App/Blocks/src/eventsComponent/edit.js
@@ -1,7 +1,10 @@
 import {
 	InnerBlocks,
+	InspectorControls,
+	InspectorAdvancedControls,
 	useBlockProps,
 } from '@wordpress/block-editor';
+import { SelectControl } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
 
 import classNames from 'classnames';
@@ -42,6 +45,50 @@ const Edit = ( props ) => {
 		setAttributes,
 	} = props;
 
+	const{ 
+		tagName,
+	} = attributes;
+
+	const setTagName = ( value ) => {
+		setAttributes( {
+			tagName: value
+		} );
+	}
+
+	const TagNameSelector = () => {
+
+		const htmlElementMessages = {
+			section: __(
+				"The <section> element should represent a standalone portion of the document that can't be better represented by another element."
+			),
+			div: __(
+				"The <div> element is the generic container for flow content."
+			),
+		};
+
+		const options = [
+			{ label: 'Default (<section>)', value: 'section' },
+			{ label: __( '<div>' ), value: 'div' },
+		];
+
+		if( !options || !options.length ) {
+			return <Spinner />;
+		}
+
+		return (
+			<InspectorAdvancedControls group="advanced">
+				<SelectControl
+					__nextHasNoMarginBottom
+					label={ __( 'HTML element' ) }
+					options={ options }
+					onChange={ setTagName }
+					value={ tagName }
+					help={ htmlElementMessages[ tagName ] }
+				/>
+			</InspectorAdvancedControls>
+		);
+	};
+
 	const blockProps = useBlockProps( {
 		className: classNames( className, 'events' ),
 	} );
@@ -49,6 +96,7 @@ const Edit = ( props ) => {
 	return (
 		<div
 			{ ...blockProps } >
+			<TagNameSelector />
 			<InnerBlocks
 				allowedBlocks={ ALLOWED_BLOCKS }
 				template={ TEMPLATE }
